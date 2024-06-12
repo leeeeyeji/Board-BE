7bfcbf829854182fc41ea9ff46eb8b9eb779f6ee
yeji <dldpwl1705@naver.com>
2024-06-12 11:23:33 +0900

    연관관계 맵핑 완려

--- src/main/java/yeseung/board/post/Post.java
+++ src/main/java/yeseung/board/post/Post.java
@@ -2,9 +2,12 @@ package yeseung.board.post;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import yeseung.board.comment.Comment;
 import yeseung.board.member.Member;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -25,4 +28,7 @@ public class Post {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
+
+    @OneToMany(mappedBy = "post" , cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> commentList = new ArrayList<>();
 }
